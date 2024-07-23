cc7ea7eabdefc52f5bb19d52f0e60d1c3f0e2e32
wintery39 <wintery39@naver.com>
2024-07-23 21:49:03 +0900

    image start

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,9 +1,11 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Req } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Req, UseInterceptors, UploadedFile, UnsupportedMediaTypeException, ForbiddenException, InternalServerErrorException } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { AuthGuard } from 'src/auth/security/auth.guard';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage, FileFilterCallback } from 'multer';
 
 @ApiTags('Users')
 @Controller('users')
@@ -14,6 +16,37 @@ export class UsersController {
   @ApiOperation({ summary: '모든 유저 조회(개발용으로 사용)' })
   findAll() {
     return this.usersService.findAll();
+  }
+
+  @Post('/photo')
+  @UseInterceptors(FileInterceptor('image',{
+    fileFilter: (req: Request, file, cb: FileFilterCallback) => {
+      if(!file.mimetype.match(/image\/(jpg|jpeg|png)/)){
+        return cb(new UnsupportedMediaTypeException(
+          'jpg, jpeg, png형식의 파일만 업로드 가능합니다.'
+        ),);
+      }
+      cb(null, true);
+  },
+  storage: diskStorage({
+    destination: './profile/images',
+    filename: (req, file, cb) => {
+      const extArray = file.mimetype.split('/');
+
+      const date_now = `${Date.now()}`;
+      cb(null, date_now + '.' + extArray[extArray.length - 1]);
+    },
+  }),
+  limits: { fileSize: 5 * 1024 * 1024 },
+  }))
+  @ApiOperation({ summary: '이미지 업로드//개발중' })
+  @UseGuards(AuthGuard)
+  @ApiBearerAuth('token')
+  uploadImage(@Req() req: Request, @UploadedFile() image: Express.Multer.File){
+    if(image === undefined){
+      throw new InternalServerErrorException('이미지를 업로드해주세요.');
+    }
+    return image.filename;
   }
 
   @Get('/mademeeting')
